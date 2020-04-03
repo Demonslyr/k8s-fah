5dfc48ba75ebd6053a7c0ff8563c9d781fc7e22a
Demonslyr <Demonslyr@users.noreply.github.com>
2020-04-03 12:22:40 -0500

    Create buildFaH.groovy

---
+++ Jenkins/buildFaH.groovy
@@ -0,0 +1,30 @@
+node {
+    stage('setup'){
+        checkout scm
+        currentBuild.description = "${Branch}"
+        appName = "k8s-fah-richstokes"
+        dockerRepo = "atriarchsystems"
+        deploymentEnvironment = "Development"
+        dockerCredId = "AtriarchDockerID"
+        dockerfilePathFromRoot = "./Dockerfile"// this is the path from the base directory
+    }
+    stage('build'){
+       def buildout = sh(returnStdout: true, script: "docker build -t ${appName} -f ${dockerfilePathFromRoot} .")
+       println buildout
+    }
+    stage('push'){
+        def tagout = sh(returnStdout: true, script: "docker tag ${appName} ${dockerRepo}/${appName}:latest")
+        println tagout
+        withCredentials([usernamePassword(usernameVariable: "DOCKER_USER",passwordVariable: "DOCKER_PASS", credentialsId: dockerCredId)]){
+            def loginout = sh(returnStdout: true, script: "echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin")
+            println loginout
+            def pushout = sh(returnStdout: true, script: "docker push ${dockerRepo}/${appName}:latest")
+            println pushout
+        }
+    }
+    stage('deploy'){        
+        def deployout = sh(returnStdout: true, script: '''kubectl apply -f ./folding-cpu.yaml -n k8s-fah''')
+        //def deployout = sh(returnStdout: true, script: '''kubectl patch deployment drinkpointweb-deployment -n k8s-fah -p "{\\"spec\\":{\\"template\\":{\\"metadata\\":{\\"labels\\":{\\"date\\":\\"`date +'%s'`\\"}}}}}"''')
+        println deployout        
+    }                
+}
